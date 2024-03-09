79b263a467327a57e61b3298a0b55bdad8d8fec4
partha <partharaj.dev@gmail.com>
2024-03-09 20:10:21 +0600

    Initial commit

---
+++ src/main/java/org/wsd/app/config/Config.java
@@ -0,0 +1,2 @@
+package org.wsd.app.config;public class Config {
+}
---
+++ src/test/java/org/wsd/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wsd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
